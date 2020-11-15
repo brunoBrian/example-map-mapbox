349352f2684e1fe1a7fc4e2e9a71d9fde4ecda09
Bruno Brian da Silva Sousa <bruno.brian@BRSPOAMEANM0005.local>
2020-11-15 20:39:44 -0300

    change url

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ export default function App() {
       <ReactMapGL
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        mapStyle="mapbox://styles/leighhalliday/cjufmjn1r2kic1fl9wxg7u1l4"
+        mapStyle="mapbox://styles/brunobrian/ckhjqtnqc1cu119ru1jrosx47"
         onViewportChange={viewport => {
           setViewport(viewport);
         }}
